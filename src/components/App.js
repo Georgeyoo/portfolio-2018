85478657c89f5cf80b2a448375fa19659ed5bfe4
George Yoo <georgeyoo@Georges-MacBook-Pro-2.local>
2018-06-02 00:33:42 -0700

    Added router dom and created links to components

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './main';
+import { Link } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+          <Layout>
+              <Header title="/G/eorge Yoo" scroll>
+                  <Navigation>
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/aboutme">About Me</Link>
+                      <Link to="/projects">Projects</Link>
+                      <Link to="/contact">Contact</Link>
+                  </Navigation>
+              </Header>
+              <Drawer title="/G/eorge Yoo">
+                  <Navigation>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/aboutme">About Me</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link>
+                  </Navigation>
+              </Drawer>
+              <Content>
+                  <div className="page-content" />
+                  <Main/>
+              </Content>
+          </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
