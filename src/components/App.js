cfe2cd3714555c0d47522dad3a0adb141420703f
George Yoo <georgeyoo@Georges-MacBook-Pro-2.local>
2018-06-02 17:38:58 -0700

    Projects page template complete

--- src/components/App.js
+++ src/components/App.js
@@ -7,30 +7,32 @@ import { Link } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <div className="demo-big-content">
-          <Layout>
-              <Header className="header-color" title="/G/eorge Yoo" scroll>
-                  <Navigation>
-                      <Link to="/resume">Resume</Link>
-                      <Link to="/aboutme">About Me</Link>
-                      <Link to="/projects">Projects</Link>
-                      <Link to="/contact">Contact</Link>
-                  </Navigation>
-              </Header>
-              <Drawer title="/G/eorge Yoo">
-                  <Navigation>
-                    <Link to="/resume">Resume</Link>
-                    <Link to="/aboutme">About Me</Link>
-                    <Link to="/projects">Projects</Link>
-                    <Link to="/contact">Contact</Link>
-                  </Navigation>
-              </Drawer>
-              <Content>
-                  <div className="page-content" />
-                  <Main/>
-              </Content>
-          </Layout>
-      </div>
+        <div className="demo-big-content">
+            <Layout>
+                <Header className="header-color" title="/G/eorge Yoo" scroll>
+                    <Navigation>
+                        <Link to="/">Home</Link>
+                        <Link to="/resume">Resume</Link>
+                        <Link to="/aboutme">About Me</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/contact">Contact</Link>
+                    </Navigation>
+                </Header>
+                <Drawer title="/G/eorge Yoo">
+                    <Navigation>
+                        <Link to="/">Home</Link>
+                        <Link to="/resume">Resume</Link>
+                        <Link to="/aboutme">About Me</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/contact">Contact</Link>
+                    </Navigation>
+                </Drawer>
+                <Content>
+                    <div className="page-content" />
+                    <Main/>
+                </Content>
+            </Layout>
+        </div>
     );
   }
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,85 @@
 import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeTab: 0
+        }
+    }
+
+    toggleCategories() {
+        if(this.state.activeTab === 0) {
+            return(
+                <div className="projects-grid">
+                    {/* Project 1 */}
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{color: 'white', height: '176px', background: 'url(https://reactjs.org/logo-og.png) center / cover'}}> React Project #1</CardTitle>
+                        <CardText> Lorem Ipsum </CardText>
+                        <CardActions border>
+                            <Button colored>Repository</Button>
+                            <Button colored>Deployed</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name='share'/>
+                        </CardMenu>
+                    </Card>
+
+                    {/* Project 2 */}
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{color: 'white', height: '176px', background: 'url(https://reactjs.org/logo-og.png) center / cover'}}> React Project #2</CardTitle>
+                        <CardText> Lorem Ipsum </CardText>
+                        <CardActions border>
+                            <Button colored>Repository</Button>
+                            <Button colored>Deployed</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name='share'/>
+                        </CardMenu>
+                    </Card>
+
+                    {/* Project 3 */}
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{color: 'white', height: '176px', background: 'url(https://reactjs.org/logo-og.png) center / cover'}}> React Project #3</CardTitle>
+                        <CardText> Lorem Ipsum </CardText>
+                        <CardActions border>
+                            <Button colored>Repository</Button>
+                            <Button colored>Deployed</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name='share'/>
+                        </CardMenu>
+                    </Card>
+                </div>
+            )
+        }
+        else if(this.state.activeTab === 1) {
+            return(
+                <div><h1>This is more React!</h1></div>
+            )
+        }
+        else if(this.state.activeTab === 2) {
+            return(
+                <div><h1>This is the last React category!</h1></div>
+            )
+        }
+    }
+
     render() {
         return (
-            <div>
-                <h1> Projects </h1>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
+                    <Tab>React</Tab>
+                    <Tab>React</Tab>
+                    <Tab>React</Tab>
+                </Tabs>
+
+                <Grid>
+                    <Cell col={12}>
+                        <div className="content">{this.toggleCategories()}</div> 
+                    </Cell>
+                </Grid>
             </div>
         );
     }
