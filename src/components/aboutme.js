6cb23df6989086dc1cc9c159c911f54c546d851b
George <gcyoo@ucla.edu>
2018-06-12 23:14:57 -0700

    Added profile pic file and started about me page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Aboutme extends Component {
     render() {
         return (
-            <div>
-                <h1> Aboutme </h1>
+            <div className="about-body">
+                <Grid className="about-grid">
+                    <Cell col={12}>
+                        <img
+                            src={require("./ProfilePic.jpg")}
+                            alt="george-profile-pic"
+                            style={{height: '250px', borderRadius: '25px'}}
+                        />
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum</p>
+                    </Cell>
+                </Grid>
             </div>
         );
     }
